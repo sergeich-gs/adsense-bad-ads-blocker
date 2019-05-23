1bf6a98834c730da2a113d5d945054f8ffa04052
sergeich-gs <42623540+sergeich-gs@users.noreply.github.com>
2019-05-23 14:29:44 +0300

    Update cron_settings.php

--- cron_settings.php
+++ cron_settings.php
@@ -31,23 +31,17 @@ $script_folder = __DIR__;
 $cron_settings = <<<CRON_SET
 
 
-# Check 50 new text ads:<br />
-2,4,8,13,53,58  *  *  *  * $username /usr/bin/php $script_folder/search_bad_ads.php '!!!{"num_of_pages":"1","num_of_ads_per_page":"50","text":"checked","stopwords_check":"checked","whitelist":"checked","lat2cyr":"checked","ad_account":"checked","blogspot":"checked","disguised_text":"checked","too_many_spaces":"checked","check_target_url":"checked","mark_reviewed":"checked","get_stats":"checked","no_save_clear":"checked","report_words":"checked","report_disg":"checked","arc":"arc5"}'
+2,4,8,13,53,58  *  *  *  * $username /usr/bin/php $script_folder/cron1.php
 <br /><br />
-# Check 50 new media ads:<br />
-1,3,6,12,52,59  *  *  *  * $username /usr/bin/php $script_folder/search_bad_ads.php '!!!{"num_of_pages":"1","num_of_ads_per_page":"50","rich_media":"checked","stopwords_check":"checked","whitelist":"checked","lat2cyr":"checked","ad_account":"checked","blogspot":"checked","too_many_spaces":"checked","check_target_url":"checked","mark_reviewed":"checked","get_stats":"checked","no_save_clear":"checked","report_words":"checked","report_disg":"checked","arc":"arc5"}'
+1,3,6,12,52,59  *  *  *  * $username /usr/bin/php $script_folder/cron2.php
 <br /><br />
-# Check 50 new image ads:<br />
-12,20,25,35,40,51  *  *  *  * $username /usr/bin/php $script_folder/search_bad_ads.php '!!!{"num_of_pages":"1","num_of_ads_per_page":"50","image":"checked","stopwords_check":"checked","whitelist":"checked","ad_account":"checked","blogspot":"checked","check_target_url":"checked","mark_reviewed":"checked","get_stats":"checked","report_words":"checked","no_save_clear":"checked","arc":"arc5"}'
+12,20,25,35,40,51  *  *  *  * $username /usr/bin/php $script_folder/cron3.php
 <br /><br />
-# Check 250 (5 pages by 50 ads) already checked media ads:<br />
-7,11,22,33,39,49  *  *  *  * $username /usr/bin/php $script_folder/search_bad_ads.php '!!!{"num_of_pages":"5","num_of_ads_per_page":"50","rich_media":"checked","stopwords_check":"checked","whitelist":"checked","lat2cyr":"checked","ad_account":"checked","blogspot":"checked","too_many_spaces":"checked","check_target_url":"checked","get_stats":"checked","reviewed":"checked","no_save_clear":"checked","report_words":"checked","report_disg":"checked","arc":"arc5"}'
+7,11,22,33,39,49  *  *  *  * $username /usr/bin/php $script_folder/cron4.php
 <br /><br />
-# Check 250 (5 pages by 50 ads) already checked text ads:<br />
-2,17,27,38,47,54  *  *  *  * $username /usr/bin/php $script_folder/search_bad_ads.php '!!!{"num_of_pages":"5","num_of_ads_per_page":"50","text":"checked","stopwords_check":"checked","redirects_text":"checked","badadlist_check":"checked","whitelist":"checked","lat2cyr":"checked","reviewed":"checked","ad_account":"checked","blogspot":"checked","too_many_spaces":"checked","disguised_text":"checked","check_target_url":"checked","get_stats":"checked","no_save_clear":"checked","report_words":"checked","report_disg":"checked","arc":"arc5"}'
+2,17,27,38,47,54  *  *  *  * $username /usr/bin/php $script_folder/cron5.php
 <br /><br />
-# Check up to 50 ads by each word from searchword list:<br />
-15,30,45  *  *  *  * $username /usr/bin/php $script_folder/search_bad_ads.php '!!!{"num_of_pages":"5","num_of_ads_per_page":"50","text":"checked","stopwords_check":"checked","redirects_text":"checked","badadlist_check":"checked","whitelist":"checked","lat2cyr":"checked","reviewed":"checked","ad_account":"checked","blogspot":"checked","too_many_spaces":"checked","disguised_text":"checked","check_target_url":"checked","get_stats":"checked","no_save_clear":"checked","report_words":"checked","report_disg":"checked","arc":"arc5"}'
+15,30,45  *  *  *  * $username /usr/bin/php $script_folder/cron6.php
 
 
 CRON_SET;
