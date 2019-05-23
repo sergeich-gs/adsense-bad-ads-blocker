201d87a5d6b9b7afcc6bf8a9f05eb67939874dcc
sergeich-gs <42623540+sergeich-gs@users.noreply.github.com>
2019-05-23 14:44:48 +0300

    Update cron6.php

--- cron6.php
+++ cron6.php
@@ -5,6 +5,7 @@ $suffix = basename(__FILE__, '.php');
 if (file_exists($GLOBALS['settings_folder'] . "settings.$suffix.ini")) {
     $set = file_get_contents($GLOBALS['settings_folder'] . "settings.$suffix.ini");
     $set = json_decode($set, 1);
-}
+} else
+    die();
 
 include 'search_bad_ads.php';
